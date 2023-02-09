a3b339d62b97bbe2e9e24ae1dbd5c6c124c94092
jery lenas <jerylenas@gmail.com>
2023-02-09 07:54:09 +0700

    1. designing advice card 2. designing button 3. create fetch advice services

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ const button = document.querySelector(".main__card-button")
 const advice = document.querySelector('.main__card-advice')
 const id = document.querySelector('.main__card-id')
 const URL = "https://api.adviceslip.com/advice"
+
 button.addEventListener('click', async () => {
     button.disabled = true
-    advice.innerHTML = "Loading . . . "
+    advice.textContent = ". . . "
+    id.textContent="..."
     const response = await fetch(URL)
     const json = await response.json()
     const { slip } = json
-    id.innerHTML = slip.id
-    advice.innerHTML = `" ${slip.advice} "`
+    id.textContent = slip.id
+    advice.textContent = `"${slip.advice}"`
     button.disabled = false
-  
 })
